410eec6076ec930810f05f26136fe405b03393c5
unknown <munibarasheed86@gmail.com>
2020-12-17 19:58:21 +0530

    EFCoreCodeFirst

---
+++ EFCoreCodeFirst/ASP.NET CoreMVC/EFCoreCodeFirst/EFCoreCodeFirst/Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirst.Models
+{
+    public class EmployeeContext :DbContext
+    { 
+        public EmployeeContext(DbContextOptions<EmployeeContext> Options) : base(Options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
