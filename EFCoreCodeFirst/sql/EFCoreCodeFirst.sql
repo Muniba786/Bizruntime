63e03e845769fd477fac75106269870580097f9f
unknown <munibarasheed86@gmail.com>
2020-12-28 16:54:54 +0530

    firstproj

--- EFCoreCodeFirst/sql/EFCoreCodeFirst.sql
+++
@@ -1,7 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Id]
-      ,[Name]
-      ,[Gender]
-      ,[Desingnation]
-      ,[Salary]
-  FROM [CodeFirstEFCore].[dbo].[Employees]
